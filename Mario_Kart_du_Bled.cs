98d0feda107eca34875fe9863540683e13a29af7
Pierre <Yulgoat>
2023-03-16 12:04:41 +0100

    Ajout Area Zone Lente MKB

--- Mario_Kart_du_Bled.cs
+++ Mario_Kart_du_Bled.cs
@@ -3,87 +3,87 @@ using System;
 
 public class Mario_Kart_du_Bled : Node2D
 {
-      // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-    public float time = 0F;
-    public float best_time = 999F;
-    public int nb_checkpoint_passed = 0;
-    public bool[] all_passed = {false, false, false, false};
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+	  // Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	public float time = 0F;
+	public float best_time = 999F;
+	public int nb_checkpoint_passed = 0;
+	public bool[] all_passed = {false, false, false, false};
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
   {
-      time+=delta;
-      var msg = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("time");
-      msg.Text = "TIME :"+time;
-      msg.Show();
+	  time+=delta;
+	  var msg = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("time");
+	  msg.Text = "TIME :"+time;
+	  msg.Show();
   }
 
   public void _on_checkpoint1_body_entered(Node2D body)
   {
-    if(body.Name == "player"){
-        if(all_passed[0] == false){
-            all_passed[0] = true;
-            nb_checkpoint_passed++;
-            var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("nbcp");
-            msg2.Text = "CHECKPOINT PASSED :"+nb_checkpoint_passed;
-            msg2.Show();
-        }
-    }
+	if(body.Name == "player"){
+		if(all_passed[0] == false){
+			all_passed[0] = true;
+			nb_checkpoint_passed++;
+			var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("nbcp");
+			msg2.Text = "CHECKPOINT PASSED :"+nb_checkpoint_passed;
+			msg2.Show();
+		}
+	}
   }
   public void _on_checkpoint2_body_entered(Node2D body)
   {
-    if(body.Name == "player"){
-        if(all_passed[1] == false){
-            all_passed[1] = true;
-            nb_checkpoint_passed++;
-            var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("nbcp");
-            msg2.Text = "CHECKPOINT PASSED :"+nb_checkpoint_passed;
-            msg2.Show();
-        }
-    }
+	if(body.Name == "player"){
+		if(all_passed[1] == false){
+			all_passed[1] = true;
+			nb_checkpoint_passed++;
+			var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("nbcp");
+			msg2.Text = "CHECKPOINT PASSED :"+nb_checkpoint_passed;
+			msg2.Show();
+		}
+	}
   }
   public void _on_checkpoint3_body_entered(Node2D body)
   {
-    if(body.Name == "player"){
-        if(all_passed[2] == false){
-            all_passed[2] = true;
-            nb_checkpoint_passed++;
-            var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("nbcp");
-            msg2.Text = "CHECKPOINT PASSED :"+nb_checkpoint_passed;
-            msg2.Show();
-        }
-    }
+	if(body.Name == "player"){
+		if(all_passed[2] == false){
+			all_passed[2] = true;
+			nb_checkpoint_passed++;
+			var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("nbcp");
+			msg2.Text = "CHECKPOINT PASSED :"+nb_checkpoint_passed;
+			msg2.Show();
+		}
+	}
   }
   public void _on_checkpoint4_body_entered(Node2D body)
   {
-    if(body.Name == "player"){
-        if(all_passed[3] == false){
-            all_passed[3] = true;
-            nb_checkpoint_passed++;
-            var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("nbcp");
-            msg2.Text = "CHECKPOINT PASSED :"+nb_checkpoint_passed;
-            msg2.Show();
-        }
-    }
+	if(body.Name == "player"){
+		if(all_passed[3] == false){
+			all_passed[3] = true;
+			nb_checkpoint_passed++;
+			var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("nbcp");
+			msg2.Text = "CHECKPOINT PASSED :"+nb_checkpoint_passed;
+			msg2.Show();
+		}
+	}
   }
 
   public void _on_finish_line_body_entered(Node2D body)
   {
-    if(body.Name == "player"){
-        if((time < best_time) && (all_passed[0] && all_passed[1] && all_passed[2] && all_passed[3])){
-            best_time = time;
-        }
-        var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("best");
-        msg2.Text = "BEST :"+best_time;
-        msg2.Show();
-    }
+	if(body.Name == "player"){
+		if((time < best_time) && (all_passed[0] && all_passed[1] && all_passed[2] && all_passed[3])){
+			best_time = time;
+		}
+		var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("best");
+		msg2.Text = "BEST :"+best_time;
+		msg2.Show();
+	}
   }
 }
 
--- player.cs
+++ player.cs
@@ -14,33 +14,33 @@ public class player : RigidBody2D
 	[Export] 
 	public float DRIFT_STEERING=600.0F;
 
-    private IA Ia = new IA();
+	private IA Ia = new IA();
 
-    public override void _PhysicsProcess(float delta)
-    {   
-        bool[] tab = Ia.launch(this.LinearVelocity);
-        input(tab);
-    }
-    
-    public void input(bool[] tab)
-    {
-        this.LinearDamp = FRICTION;
-        if (Input.IsActionPressed("ui_left") || tab[0])
-        {
-            ApplyTorqueImpulse(-STEERING);
-        }
-        if (Input.IsActionPressed("ui_right") || tab[1])
-        {
-            ApplyTorqueImpulse(STEERING);
-        }
-        if (Input.IsActionPressed("ui_up") || tab[2])
-        {
-            ApplyCentralImpulse((new Vector2(0, -ACCELERATION)).Rotated(Rotation));
-        }
-        if (Input.IsActionPressed("ui_down") || tab[3])
-        {
-            ApplyCentralImpulse((new Vector2(0, ACCELERATION)).Rotated(Rotation));
-        }
-        
-    }
+	public override void _PhysicsProcess(float delta)
+	{   
+		bool[] tab = Ia.launch(this.LinearVelocity);
+		input(tab);
+	}
+	
+	public void input(bool[] tab)
+	{
+		this.LinearDamp = FRICTION;
+		if (Input.IsActionPressed("ui_left") || tab[0])
+		{
+			ApplyTorqueImpulse(-STEERING);
+		}
+		if (Input.IsActionPressed("ui_right") || tab[1])
+		{
+			ApplyTorqueImpulse(STEERING);
+		}
+		if (Input.IsActionPressed("ui_up") || tab[2])
+		{
+			ApplyCentralImpulse((new Vector2(0, -ACCELERATION)).Rotated(Rotation));
+		}
+		if (Input.IsActionPressed("ui_down") || tab[3])
+		{
+			ApplyCentralImpulse((new Vector2(0, ACCELERATION)).Rotated(Rotation));
+		}
+		
+	}
 }
