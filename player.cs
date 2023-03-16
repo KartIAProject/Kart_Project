90483050bb6fad12096b50bc6084f35649fffddc
Malvin Chevallier <malvin.chevallier@gmail.com>
2023-03-16 11:35:15 +0100

    Ajout début IA

--- player.cs
+++ player.cs
@@ -14,27 +14,30 @@ public class player : RigidBody2D
     [Export] 
     public float DRIFT_STEERING=600.0F;
 
+    private IA Ia = new IA();
+
     public override void _PhysicsProcess(float delta)
-    {
-        input();
+    {   
+        bool[] tab = Ia.launch(this.LinearVelocity);
+        input(tab);
     }
     
-    public void input()
+    public void input(bool[] tab)
     {
         this.LinearDamp = FRICTION;
-        if (Input.IsActionPressed("ui_left"))
+        if (Input.IsActionPressed("ui_left") || tab[0])
         {
             ApplyTorqueImpulse(-STEERING);
         }
-        if (Input.IsActionPressed("ui_right"))
+        if (Input.IsActionPressed("ui_right") || tab[1])
         {
             ApplyTorqueImpulse(STEERING);
         }
-        if (Input.IsActionPressed("ui_up"))
+        if (Input.IsActionPressed("ui_up") || tab[2])
         {
             ApplyCentralImpulse((new Vector2(0, -ACCELERATION)).Rotated(Rotation));
         }
-        if (Input.IsActionPressed("ui_down"))
+        if (Input.IsActionPressed("ui_down") || tab[3])
         {
             ApplyCentralImpulse((new Vector2(0, ACCELERATION)).Rotated(Rotation));
         }
