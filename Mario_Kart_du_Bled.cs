a0918bb5fd43bc42a27f859b49741eaaa8a4a934
Jules Chaidron <jules.chaidron@insa-rennes.fr>
2023-03-23 08:19:35 +0100

    Mario_Kart_du_Bled : script modifié - annontations et deux trois ajouts sur finish_line

--- Mario_Kart_du_Bled.cs
+++ Mario_Kart_du_Bled.cs
@@ -3,13 +3,12 @@ using System;
 
 public class Mario_Kart_du_Bled : Node2D
 {
-	  // Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+	  // Member variables
 	public float time = 0F;
 	public float best_time = 999F;
 	public int nb_checkpoint_passed = 0;
 	public bool[] all_passed = {false, false, false, false};
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -17,6 +16,7 @@ public class Mario_Kart_du_Bled : Node2D
 	}
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
+  // Each checkpoint (Area2D objects) must be passd trought at least once before the finish line allow the best time to be displayed
   public override void _Process(float delta)
   {
 	  time+=delta;
@@ -74,15 +74,28 @@ public class Mario_Kart_du_Bled : Node2D
 	}
   }
 
+// the finish line displays the best time on the car entering it's collider, ony if the lap was done "correctly".
+// it also reinitialize the timer and allows to run another lap by reinitializing the all_passed array.
   public void _on_finish_line_body_entered(Node2D body)
   {
 	if(body.Name == "player"){
 		if((time < best_time) && (all_passed[0] && all_passed[1] && all_passed[2] && all_passed[3])){
 			best_time = time;
+			time = 0F;
+			var msg = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("time");
+	  		msg.Text = "TIME :"+time;
+	  		msg.Show();
 		}
 		var msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("best");
 		msg2.Text = "BEST :"+best_time;
 		msg2.Show();
+		nb_checkpoint_passed = 0;
+		msg2 = (GetNode<CanvasLayer>("HUD")).GetNode<Label>("nbcp");
+		msg2.Text = "CHECKPOINT PASSED :"+nb_checkpoint_passed;
+		msg2.Show();
+		for(int i = 0; i<4; i++){
+			all_passed[i] = false;
+		}
 	}
   }
 }
